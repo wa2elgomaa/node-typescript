00cb166d344dc91de3dbc5543933cc8421aabc79
Wael Gomaa <wg_gomaa@hotmail.com>
2021-01-15 14:23:41 +0400

    Allow CORS domain

--- src/app.ts
+++ src/app.ts
@@ -18,6 +18,14 @@ app.use(bodyParser.json());
 
 // enable cors
 app.use(cors());
+var allowCrossDomain = function(req : express.Request, res : express.Response , next : Function) {
+    res.header("Access-Control-Allow-Origin", "*"); // allow requests from any other server
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE'); // allow these verbs
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Cache-Control");
+    next();
+}
+app.use(allowCrossDomain); // plumbing it in as middleware
+
 
 // API 
 app.use('/api', router);
